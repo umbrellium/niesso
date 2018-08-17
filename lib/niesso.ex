01ad6ae1aaac5b72e5d1632e0d3bf8e754cadbc1
Sam Mulube <sam@umbrellium.co.uk>
2018-08-17 13:56:47 +0100

    Remove previous placeholder function, and create new one

--- lib/niesso.ex
+++ lib/niesso.ex
@@ -1,18 +1,18 @@
 defmodule Niesso do
   @moduledoc """
-  Documentation for Niesso.
+  SAML helper module, intended to make interacting with NIE's SAML endpoint
+  from Elixir easier.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Niesso.hello()
-      :world
-
+  Consume response takes as input the encoded SAML response received by a
+  SAML service provider that is taking part in a SAML flow, and from this we
+  validate the data (including ultimately varifying the signature of the
+  data), and then we return an instantiated assertion struct containing
+  verified information about the user suitable for consumption by the calling
+  application.
   """
-  def hello do
-    :world
+  def consume_response(response) do
+    :assertion
   end
 end
--- test/niesso_test.exs
+++ test/niesso_test.exs
@@ -2,7 +2,8 @@ defmodule NiessoTest do
   use ExUnit.Case
   doctest Niesso
 
-  test "greets the world" do
-    assert Niesso.hello() == :world
+  test "consumes a response and returns an assertion" do
+    response = "response"
+    assert Niesso.consume_response(response) == :assertion
   end
 end

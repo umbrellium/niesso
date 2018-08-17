a0e62a364e97f5bb369b851df5b47c4e842b2a7e
Sam Mulube <sam@umbrellium.co.uk>
2018-08-17 23:27:09 +0100

    WIP - adding decoding

--- lib/niesso.ex
+++ lib/niesso.ex
@@ -3,16 +3,24 @@ defmodule Niesso do
   SAML helper module, intended to make interacting with NIE's SAML endpoint
   from Elixir easier.
   """
+  use Private
 
   @doc """
-  Consume response takes as input the encoded SAML response received by a
-  SAML service provider that is taking part in a SAML flow, and from this we
-  validate the data (including ultimately varifying the signature of the
-  data), and then we return an instantiated assertion struct containing
-  verified information about the user suitable for consumption by the calling
+  This function takes as input the saml encoding parameter, and the encoded
+  SAML response received by a SAML service provider that is taking part in a
+  SAML flow, and from this we validate the data (including ultimately
+  varifying the signature of the data), and then we return an instantiated
+  assertion struct containing verified information about the user suitable
+  for consumption by the calling
   application.
   """
-  def consume_response(response) do
+  def consume_auth_resp(saml_encoding, saml_payload) do
     :assertion
+  end
+
+  private do
+    defp decode_saml_payload(saml_encoding, saml_payload) do
+      saml_payload
+    end
   end
 end
--- test/niesso_test.exs
+++ test/niesso_test.exs
@@ -2,8 +2,22 @@ defmodule NiessoTest do
   use ExUnit.Case
   doctest Niesso
 
-  test "consumes a response and returns an assertion" do
-    response = "response"
-    assert Niesso.consume_response(response) == :assertion
+  describe "consume_auth_resp/2" do
+    test "consumes a response and returns an assertion" do
+      response = "response"
+      assert Niesso.consume_auth_resp(:undefined, response) == :assertion
+    end
+  end
+
+  describe "decoding saml payload" do
+    @encoded_payload "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4KPHNhbWwycDpSZXNwb25zZT4KPHNhbWwycDpSZXNwb25zZT4="
+
+    test "has method for decoding a saml payload" do
+      assert Niesso.decode_saml_payload(:undefined, "foo") == "foo"
+    end
+
+    test "returns parsed xml document"do
+      
+    end
   end
 end

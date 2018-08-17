541acebb44f4f30959cd79b21899b8583fb262f3
Sam Mulube <sam@umbrellium.co.uk>
2018-08-18 00:26:49 +0100

    Add sweet_xml library

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Niesso.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:private, "~> 0.1.1"}
+      {:private, "~> 0.1.1"},
+      {:sweet_xml, "~> 0.6.5"}
     ]
   end
 end

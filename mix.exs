b820935b6b16ae66f0185de6eb180694bb607d53
Sam Mulube <sam@umbrellium.co.uk>
2018-08-21 10:32:34 +0100

    Update dependencies
    Remove `private`, add `excoveralls`

--- mix.exs
+++ mix.exs
@@ -7,14 +7,16 @@ defmodule Niesso.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :timex]
     ]
   end
 
@@ -23,8 +25,10 @@ defmodule Niesso.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:private, "~> 0.1.1"},
-      {:sweet_xml, "~> 0.6.5"}
+      {:sweet_xml, "~> 0.6.5"},
+      {:timex, "~> 3.1"},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.9", only: :test}
     ]
   end
 end
